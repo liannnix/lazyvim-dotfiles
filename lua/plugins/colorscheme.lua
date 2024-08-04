0852184d799c0cee9e0a0ea4753a61fe495b3aa0
Andrey Limachko <liannnix@giran.cyou>
2024-08-04 23:05:06 +0400

    Enbale tokyonight moon theme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "moon" },
+  },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  }
+}
